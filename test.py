948a853b4e538dcaa175f577373ab162f0218111
Ben Cortez <Ben.cortez@gmail.com>
2024-05-18 11:53:25 +1000

    changed var

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 
 
 # create a list of 100 numbers from 0 to 99
-numbers = [number for number in range(100)]
+numbers1 = [number for number in range(100)]
 
 # create a list of even numbers from 0 to 99
 even_numbers = [number for number in numbers if number % 2 == 0]
